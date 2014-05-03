6a9d73c45656b5509525cc1ccb2e1c9458702970
GreagDeay <greag.deay@yandex.ru>
2014-05-04 00:01:13 +0400

    Singleton
    Singleton implementation (hopes right)

---
+++ Design Patterns/Creational Patterns/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns
+{
+    public class Singleton
+    {
+        public static Singleton Instance()
+        {
+            if (_instance == null) _instance = new Singleton();
+            return _instance;
+        }
+
+        private Singleton()
+        { }
+
+        private static Singleton _instance; 
+    }
+}
